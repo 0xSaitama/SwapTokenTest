297642311d584f4e508eb715600d4378c03f1be0
0xSaitama <arnaud.daubernatali@outlook.com>
2021-04-08 15:12:35 +0200

    delegatecall for approve

--- Desktop/UNItest/contracts/MyDefi.sol
+++ Desktop/UNItest/contracts/MyDefi.sol
@@ -1,34 +1,46 @@
-import "./IUni.sol";
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
 
-contract MyDeFiProject  {
- IUniswap uniswap;
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
+
+
+contract MyDeFiProject {
+ IUniswapV2Router02 uniswapRouter;
+
 
 
  constructor(address _uniswap) public {
-   uniswap = IUniswap(_uniswap);
+   uniswapRouter = IUniswapV2Router02(_uniswap);
  }
 
 receive() external payable { }
 
- function getbalance(address token, address owner) external view returns (uint) {
-   IERC20(token).balanceOf(owner);
+
+ function getBalance(address token, address owner) external view returns (uint) {
+   return IERC20(token).balanceOf(owner);
  }
 
- function getAllowance(address token) external view returns(uint) {
-   IERC20(token).allowance(msg.sender, address(this));
+ function getAllowance(address token,address owner, address spender) external view returns(uint) {
+   return IERC20(token).allowance(owner, spender);
+
  }
 
- function approveStack(address token, uint toAllow) external returns(bool) {
-   IERC20(token).approve(address(this), toAllow);
+ function approveStack(address token, address spender, uint value) external returns(uint)  {
+   //IERC20(token).approve(spender, value);
+   (bool success, bytes memory data) = token.delegatecall(abi.encodeWithSignature("approve(address,uint256)",spender,value));
+   require(success, "not approuved");
+   return abi.decode(data, (uint));
  }
+
   // La fonction qui va permettre le swap
  function swapTokensForEth(address token, uint amountIn, uint amountOutMin, uint deadline) external {
-   IERC20(token).transferFrom(msg.sender, address(this), amountIn); // Transfert des tokens en question au smart contract ! Il faut penser à approve ce transfert avant l’utilisation de cette fonction
+   IERC20(token).transferFrom(msg.sender, address(this), amountIn);// Transfert des tokens en question au smart contract ! Il faut penser à approve ce transfert avant l’utilisation de cette fonction
    address[] memory path = new address[](2); // Création du path
    path[0] = address(token); // initialisation du path avec l'address du token à échanger
-   path[1] = uniswap.WETH(); // initialisation du path avec l'address du WETH d'Uniswap
-   IERC20(token).approve(address(uniswap), amountIn); // autoriser uniswap à utiliser nos tokens
-   uniswap.swapExactTokensForETH(
+   path[1] = uniswapRouter.WETH(); // initialisation du path avec l'address du WETH d'Uniswap
+   IERC20(token).approve(address(uniswapRouter), amountIn); // autoriser uniswap à utiliser nos tokens
+   uniswapRouter.swapExactTokensForETH(
      amountIn,
      amountOutMin,
      path,
--- Desktop/UNItest/migrations/2_Deploy_Contracts.js
+++ Desktop/UNItest/migrations/2_Deploy_Contracts.js
@@ -8,10 +8,10 @@ module.exports = async function(deployer, _network, accounts) {
  const uniRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
  const wethAddress = "0xf3a6679b266899042276804930b3bfbaf807f15b";
  await deployer.deploy(MyDeFiProject, uniRouter);
-  // const app = await MyDeFiProject.deployed();
- // await MyDeFi.approveStack(daiAddress, 10);
+// const MyDeFi = await MyDeFiProject.deployed();
+ // await MyDeFi.approveStack(daiAddress,MyDeFi.address,10);
  // const balanceWBefore = await MyDeFi.getbalance(wethAddress, accounts[0]);
- // const balanceDBefore = await MyDeFi.getbalance(daiAddress, accounts[0]);
+ // const balanceDBefore = await MyDeFi.getBalance(daiAddress, accounts[0]);
  //
  // await MyDeFi.swapTokensForEth(daiAddress, 10, Amountmin , 60);
  // const balanceWAfter = await MyDeFi.getbalance(wethAddress, accounts[0]);
